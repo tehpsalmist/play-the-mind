b72ef3748eebede73de8512d344dcd1791306e47
tehpsalmist <bstewardcodes@gmail.com>
2019-07-09 17:36:28 -0400

    apollo subscriptions and token updates under control

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+import { useApolloClient } from './useApolloClient'
+
+export {
+  useApolloClient
+}
---
+++ src/hooks/useApolloClient.js
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import ApolloClient from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+const cache = new InMemoryCache()
+const initialToken = window.localStorage.getItem('idToken')
+
+const createClient = token => {
+  // Create an http link:
+  const httpLink = new HttpLink({
+    uri: 'https://the-mind.herokuapp.com/v1/graphql'
+  })
+
+  // Create a WebSocket link:
+  const wsLink = new WebSocketLink({
+    uri: `wss://the-mind.herokuapp.com/v1/graphql`,
+    options: {
+      reconnect: true,
+      connectionParams: () => ({
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }),
+      lazy: true
+    }
+  })
+
+  // using the ability to split links, you can send data to each link
+  // depending on what kind of operation is being sent
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      )
+    },
+    wsLink,
+    httpLink
+  )
+
+  return new ApolloClient({
+    link: link,
+    cache,
+    resolvers: {}
+  })
+}
+
+const firstClient = createClient(initialToken)
+
+firstClient.writeData({
+  data: {
+    idToken: initialToken
+  }
+})
+
+export const useApolloClient = () => {
+  const [client, setClient] = useState(firstClient)
+
+  return [client, newToken => {
+    const newClient = createClient(newToken)
+
+    newClient.writeData({
+      data: {
+        idToken: newToken
+      }
+    })
+
+    return setClient(newClient)
+  }]
+}
