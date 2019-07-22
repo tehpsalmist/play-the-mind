d1f3cc12dc6cc20ff1630758a6ac5ebd2f6b21e5
tehpsalmist <bstewardcodes@gmail.com>
2019-07-22 03:55:28 -0400

    more build tuning

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,11 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 const { NODE_ENV } = process.env
 const productionPlugins = NODE_ENV === 'production'
   ? [
-    purgecss({ content: ['./index.html', './src/**/*.js'], whitelistPatterns: [/partner-animation-\d-\d/] })
+    purgecss({
+      content: ['./index.html', './src/**/*.js'],
+      whitelistPatterns: [/partner-animation-\d-\d/],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
   ]
   : []
 
