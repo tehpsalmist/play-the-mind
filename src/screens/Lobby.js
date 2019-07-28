1ca3efe746bae9618a206820956f20be72a03428
tehpsalmist <bstewardcodes@gmail.com>
2019-07-28 04:07:06 -0400

    Lobby improvements

--- src/screens/Lobby.js
+++ src/screens/Lobby.js
@@ -8,7 +8,7 @@ import { Chat } from '../components'
 
 const USER_GAMES = gql`
   subscription games($userId: String) {
-    games(where: {players: {user_id: {_eq: $userId}}}) {
+    games(where: {players: {user_id: {_eq: $userId}}}, order_by: {created_at: desc}) {
       id
       name
       player_count
@@ -23,6 +23,7 @@ const USER_GAMES = gql`
       stars
       created_at
       finished
+      started
       round {
         id
         number_of_cards
@@ -34,7 +35,7 @@ const USER_GAMES = gql`
 
 const AVAILABLE_GAMES = gql`
   subscription games($userId: String) {
-    games(where: {_not: {players: {user_id: {_eq: $userId}}}, is_full: {_eq: false}}) {
+    games(where: {_not: {players: {user_id: {_eq: $userId}}}, is_full: {_eq: false}}, order_by: {created_at: desc}) {
       id
       name
       player_count
@@ -80,20 +81,74 @@ export const Lobby = ({ history }) => {
     {client => <>
       <p className='text-center'>Play the Mind. Become the Mind.</p>
       {!authToken ? <PleaseLogin /> : <>
-        <ul className='w-full flex flex-col-reverse items-stretch'>
+        <ul className='w-full flex flex-col items-stretch'>
+          <li className='p-2 text-green-500'>Your Games:</li>
+          <Subscription subscription={USER_GAMES} variables={{ userId: user && user.sub }}>
+            {({ loading, error, data }) => {
+              if (error) return `Error: ${error.message}`
+              if (loading) return 'Loading Your Games...'
+              if (!data || !data.games || !data.games.length) return <li className='p-2 text-red-500'>No Games Joined or Created.</li>
+
+              return (<>
+                {data.games
+                  .map(game => <li
+                    to={`/game/${game.id}`}
+                    className='p-3 m-1 flex justify-between items-center border rounded-lg'
+                    key={game.id}
+                  >
+                    <span>{game.name}</span>
+                    <strong className='ml-2'>{game.players.length}/{game.player_count}</strong>
+                    <span className='mx-auto hidden sm:flex sm:flex-wrap'>
+                      <em className='mx-1'>Players:</em>
+                      {game.players.map(p => <strong className='mx-1' key={p.id}>{p.name}</strong>)}
+                    </span>
+                    <Link className='mr-2 p-2 bg-green-500 text-white rounded' to={`/game/${game.id}`}>
+                      {game.finished ? 'View' : 'Play'}
+                    </Link>
+                    {!game.started && <Mutation mutation={LEAVE_GAME}>
+                      {(leaveGame, { loading }) => {
+                        const isOwned = game.owner_id === user.sub
+
+                        return <button
+                          className={`p-2 ${loading ? 'bg-gray-600' : isOwned ? 'bg-red-600' : 'bg-orange-500'} text-white rounded`}
+                          onClick={e => {
+                            e.preventDefault()
+
+                            !loading && leaveGame({
+                              variables: { playerId: game.players.find(p => p.user_id === user.sub).id }
+                            })
+                          }}
+                        >
+                          {isOwned ? 'Delete' : 'Leave'}
+                        </button>
+                      }}
+                    </Mutation>}
+                  </li>)}
+              </>)
+            }}
+          </Subscription>
+          <li className='p-2 text-green-500'>Available Games:</li>
           <Subscription subscription={AVAILABLE_GAMES} variables={{ userId: user && user.sub }}>
             {({ loading, error, data }) => {
               if (error) {
                 console.log(error)
                 return null
               }
-              if (loading || error || !data) return null
+              if (loading) return 'Searching for Games...'
+              if (!data || !data.games || !data.games.length) {
+                return <li className='p-2 text-red-500'>
+                  No games available right now.
+                  <br />
+                  <Link to='/new-game' className='text-purple-500'>Create a Game</Link>
+                  &nbsp;and invite a friend!
+                </li>
+              }
 
               return (<>
                 {data.games
                   .map(game => <li
                     to={`/game/${game.id}`}
-                    className='p-3 m-1 flex justify-between items-center border rounded-lg cursor-pointer'
+                    className='p-3 m-1 flex justify-between items-center border rounded-lg'
                     key={game.id}
                   >
                     <span>{game.name}</span>
@@ -126,49 +181,6 @@ export const Lobby = ({ history }) => {
                         </button>
                       }}
                     </Mutation>
-                  </li>)}
-              </>)
-            }}
-          </Subscription>
-          <Subscription subscription={USER_GAMES} variables={{ userId: user && user.sub }}>
-            {({ loading, error, data }) => {
-              if (error) return `Error: ${error.message}`
-              if (loading || !data) return 'Finding Games...'
-
-              return (<>
-                {data.games
-                  .map(game => <li
-                    to={`/game/${game.id}`}
-                    className='p-3 m-1 flex justify-between items-center border rounded-lg cursor-pointer'
-                    key={game.id}
-                  >
-                    <span>{game.name}</span>
-                    <strong className='ml-2'>{game.players.length}/{game.player_count}</strong>
-                    <span className='mx-auto hidden sm:flex sm:flex-wrap'>
-                      <em className='mx-1'>Players:</em>
-                      {game.players.map(p => <strong className='mx-1' key={p.id}>{p.name}</strong>)}
-                    </span>
-                    <Link className='mr-2 p-2 bg-green-500 text-white rounded' to={`/game/${game.id}`}>
-                      {game.finished ? 'View' : 'Play'}
-                    </Link>
-                    {!game.finished && <Mutation mutation={LEAVE_GAME}>
-                      {(leaveGame, { loading }) => {
-                        const isOwned = game.owner_id === user.sub
-
-                        return <button
-                          className={`p-2 ${loading ? 'bg-gray-600' : isOwned ? 'bg-red-600' : 'bg-orange-500'} text-white rounded`}
-                          onClick={e => {
-                            e.preventDefault()
-
-                            !loading && leaveGame({
-                              variables: { playerId: game.players.find(p => p.user_id === user.sub).id }
-                            })
-                          }}
-                        >
-                          {isOwned ? 'Delete' : 'Leave'}
-                        </button>
-                      }}
-                    </Mutation>}
                   </li>)}
               </>)
             }}
